8d659e19b81b32da4913c318502bbddd79178062
C21111222 <corentin.calmels@outlook.com>
2023-10-24 11:20:08 +0200

    improve main func

--- TP2.c
+++ TP2.c
@@ -482,58 +482,61 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    if (strcmp("1", argv[1]) == 0) {
-        if (argc < 4) {
+    switch (atoi(argv[1])) {
+        case 1: {
+            if (argc < 4) {
+                display_help();
+                return 1;
+            }
+            int size_int = atoi(argv[2]);
+            double probability_double = atof(argv[4]);
+            bool diagonal = false;
+            bool pin = false;
+            if (strcmp("true", argv[3]) == 0) diagonal = true;
+            if (strcmp("true",argv[5])==0) pin = true;
+            else diagonal = false;
+            graphe_t graphe = exo_coloration_step1(size_int, probability_double, diagonal);
+            int *couleurs = calloc(graphe.nbr_sommets, sizeof(int));
+            int nbr_couleurs = exo_coloration_step2(graphe, couleurs);
+            FILE *f = fopen("exemple2.dot", "w");   
+            write_graphviz3(f, graphe, couleurs, pin);
+            fclose(f);
+            detruire_graphe(&graphe);
+            free(couleurs);
+            fprintf(stdout,"\n Nombre de couleur : %d\n",nbr_couleurs);
+            fprintf(stdout,"\n make display pour afficher le graphe\n");
+            return 0;
+        }
+        case 2: {
+            if (argc < 4) {
+                display_help();
+                return 1;
+            }
+            int size_int = atoi(argv[2]);
+            double probability_double = atof(argv[4]);
+            bool diagonal;
+            if (strcmp("true", argv[3]) == 0) diagonal = true;
+            else diagonal = false;
+            fprintf(stdout, "\n La moyenne du nombre de parties connexes pour une grille de %d*%d et un probabilité de %f : %f\n",size_int, size_int, probability_double, moyenne_couleur(size_int, probability_double,1000, diagonal));
+            return 0;
+        }
+        case 3: {
+            if (argc < 4) {
+                display_help();
+                return 1;
+            }
+            int size_int = atoi(argv[2]);
+            int k_int = atoi(argv[4]);
+            bool diagonal;
+            if (strcmp("true", argv[3]) == 0) diagonal = true;
+            else diagonal = false;
+            fprintf(stdout, "\nLa probailité optimale pour avoir %d parties connexes à partir d'une grille %d*%d est : %f\n",k_int,size_int,size_int,prob_optimale(size_int, k_int, diagonal));
+            return 0;
+        }
+        default: {
             display_help();
             return 1;
         }
-        int size_int = atoi(argv[2]);
-        double probability_double = atof(argv[4]);
-        bool diagonal = false;
-        bool pin = false;
-        if (strcmp("true", argv[3]) == 0) diagonal = true;
-        if (strcmp("true",argv[5])==0) pin = true;
-        else diagonal = false;
-        graphe_t graphe = exo_coloration_step1(size_int, probability_double, diagonal);
-        int *couleurs = malloc(graphe.nbr_sommets * sizeof(int));
-        int nbr_couleurs = exo_coloration_step2(graphe, couleurs);
-        FILE *f = fopen("exemple2.dot", "w");
-        write_graphviz3(f, graphe, couleurs, pin);
-        fclose(f);
-        detruire_graphe(&graphe);
-        free(couleurs);
-        fprintf(stdout,"\n Nombre de couleur : %d\n",nbr_couleurs);
-        fprintf(stdout,"\n make display pour afficher le graphe\n");
-        return 0;
     }
-
-    if (strcmp("2", argv[1]) == 0) {
-        if (argc < 4) {
-            display_help();
-            return 1;
-        }
-        int size_int = atoi(argv[2]);
-        double probability_double = atof(argv[4]);
-        bool diagonal;
-        if (strcmp("true", argv[3]) == 0) diagonal = true;
-        else diagonal = false;
-        fprintf(stdout, "\n La moyenne du nombre de parties connexes pour une grille de %d*%d et un probabilité de %f : %f\n",size_int, size_int, probability_double, moyenne_couleur(size_int, probability_double,1000, diagonal));
-        return 0;
-    }
-
-    if (strcmp("3", argv[1]) == 0) {
-        if (argc < 4) {
-            display_help();
-            return 1;
-        }
-        int size_int = atoi(argv[2]);
-        int k_int = atoi(argv[4]);
-        bool diagonal;
-        if (strcmp("true", argv[3]) == 0) diagonal = true;
-        else diagonal = false;
-        fprintf(stdout, "\nLa probailité optimale pour avoir %d parties connexes à partir d'une grille %d*%d est : %f\n",k_int,size_int,size_int,prob_optimale(size_int, k_int, diagonal));
-        return 0;
-    }
-    
 }
  
